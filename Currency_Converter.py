280a018cf402fa3b7dc1da4e5057ca1aa0a6ec66
Viswa-Prakash <prakashviswa1990@gmail.com>
2023-08-27 21:17:05 +0530

    Currency_Converter.py

---
+++ Currency_Converter.py
@@ -0,0 +1,27 @@
+def convert_currency(amount, exchange_rate):
+    converted_amount = amount * exchange_rate
+    return converted_amount
+
+print("Enter the exchange: ")
+print("1.INR to USD")
+print("2.INR to EURO")
+print("3.INR to DIRHAM")
+
+choice = int(input("Enter the choice of currency exchange: "))
+inr_amount = float(input("Enter the amount: "))
+
+if choice == 1:
+    exchange_rate = 0.012
+    converted_amount = convert_currency(inr_amount, exchange_rate)
+    print(f"Equivalant amount in USD is: {converted_amount} Dollars")
+
+if choice == 2:
+    exchange_rate = 0.011
+    converted_amount = convert_currency(inr_amount, exchange_rate)
+    print(f"Equivalant amount in EURO is: {converted_amount} Euros")
+
+if choice == 3:
+    exchange_rate = 0.044
+    converted_amount = convert_currency(inr_amount, exchange_rate)
+    print(f"Equivalant amount in DIRHAM is: {converted_amount} Dirhams")
+
---
+++ To_Do_List_App.py
@@ -0,0 +1,49 @@
+tasks = []
+
+def add_task(task):
+    tasks.append(task)
+
+def view_task():
+    print("Tasks:")
+    for index, task in enumerate(tasks, start=1):
+        print(f"{index}: {task}")
+
+def remove_task():
+    tasks.pop()
+    
+
+while True:
+    print(" 1. Add Task")
+    print(" 2. View Task")
+    print(" 3. Remove Task")
+    print(" 4. Exit Task")
+
+    choice = input ("Enter your choice: ")
+
+    if choice == '1':
+        new_task = input("Enter new task: ")
+        add_task(new_task)
+        print("The task has been added successfully")
+
+    elif choice == '2':
+        view_task()
+    
+    elif choice == '3':
+        remove = int(input("Enter task number to remove: "))
+        remove-=1
+        tasks.pop(remove)
+        
+        print("The task has been removed successfully")
+        
+    elif choice == '4':
+        print("Bye")
+        break
+
+    else:
+        print("Invalid choice, Please try again")
+
+
+
+
+
+
